8c237dfa3f10a29acf474bed50a208f1dd1b752b
sourabh1428 <sppathak1428@gmail.com>
2023-11-22 00:21:08 +0530

    sda

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css'
 
  const Cards = ({details,setState,state}) => {
+    const[state2,setState2]=useState(false);
+    
     function handleClick(){
         console.log(details);
         setState(state.filter((e)=>details!==e));
+    }
+    function hue(){
+        setState2(!state2);
+    }
+    function perform(str){
+       let k= str.split(" ");
+       let i=0;
+       let ans="";
+       while(i<k.length && i<200){
+        ans=ans+" "+k[i++];
+       }
+      console.log(ans);
+      return ans;
     }
 
 
@@ -12,6 +27,9 @@ import '../styles/App.css'
     return (
     <div className='card'>
         {details.name}
+        
+        <button className='tour-item-para' onClick={hue}>show/hide</button>
+        <p>{state2&&(<p>{perform(details.info)}</p>)}</p>
         <button className='delete-btn' onClick={handleClick}>Delete</button>
     
     </div>
